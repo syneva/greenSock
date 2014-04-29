acc4ab4f70ced3b4b9a7c0df32ea941975dbfead
jcoombs3 <jcoombs@sapient.com>
2014-04-29 15:46:06 -0400

    added sprite animation
    idle and when clicked (not functional)

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,209 @@
-//write main script here
+var splatScene = {
+	init : function(){
+		splatScene.scene = new THREE.Scene();
+		splatScene.camera =  new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+		splatScene.renderer =  new THREE.WebGLRenderer();
+		splatScene.renderer.setSize( window.innerWidth, window.innerHeight );
+		document.body.appendChild( splatScene.renderer.domElement );
+		splatScene.camera.position.z = 5;
+
+		var light = splatScene.letThereBeLight();
+		splatScene.scene.add(light);
+
+		splatScene.createObjects();
+
+		for(var i = 0; i<splatScene.objects.length; i++){
+			var focus = splatScene.objects[i].getMesh();
+			splatScene.scene.add(focus);
+		}
+
+		//splatScene.scene.add(floor);
+		splatScene.render();
+	},
+
+	letThereBeLight: function(){
+		var light = new THREE.DirectionalLight( 0xffffff );
+		light.position.set( 0.5, 1, 1 );
+		light.position.normalize();
+		return light;
+	},
+
+	skybox: function(){
+	},
+
+	floor: function(){
+	},
+
+	createObjects: function(){
+
+		splatScene.objects = [];
+
+		var sphereMaterial = new THREE.MeshPhongMaterial({
+	      			// light
+	        		specular: '#FFCEB3',
+	        		// intermediate
+	        		color: '#EECEB3',
+	       			// dark
+	        		emissive: '#FFCEB3',
+	        		shininess: 50
+	      		});
+
+		var sphere = new THREE.Mesh(new THREE.SphereGeometry(.5,0,0), sphereMaterial);
+		sphere.overdraw = true;
+		sphere.position = new THREE.Vector3(0,0,0);
+		splatScene.scene.add(sphere);
+	},
+
+	//wrapper to simplify animations on objects
+	object : function(dirX, dirY, dirZ, mesh, camera){ 
+		this.dirX = dirX;
+		this.dirY = dirY;
+		this.dirZ = dirZ;
+		this.mesh = mesh;
+		this.camera = camera;
+
+		this.boxX = 5;
+		this.boxY = 5;
+		this.boxZ = 5;
+
+		this.setDirX = function(dir){
+			this.dirX = dir;
+		};
+		this.setDirY = function(dir){
+			this.dirY = dir;
+		}
+		this.setDirZ= function(dir){
+			this.dirZ = dir;
+		}
+
+		this.getDirX = function(){
+			return dirX;
+		}
+		this.getDirY = function(){
+			return dirY;
+		}
+		this.getDirZ = function(){
+			return dirZ;
+		}
+		this.getMesh = function(){
+			return this.mesh;
+		}
+		this.getCamera = function(){
+			return this.camera;
+		}
+
+		this.checkBoundingBox = function(field, value){
+			var limit;
+			var value = Math.abs(value);
+			if(field == 'x'){
+				limit = this.boxX;
+			}
+			else if(field =='y'){
+				limit = this.boxY;
+
+			}
+			else{
+				limit = this.boxX;
+			}
+
+			return (value>limit) ? false : true;			
+		};
+	},
+
+	 
+	animations : {
+		//rotation
+		rotate : function(focus){
+			var focusMesh = focus.getMesh();
+			focusMesh.rotation.x += 0.01;
+			focusMesh.rotation.y += 0.01;
+		},
+		translate : function(focus){
+			var focusMesh = focus.getMesh();
+
+			//randomize inc value
+			var randX = Math.random()/100.0*focus.getDirX();
+			var randY = Math.random()/100.0*focus.getDirY();
+			var randZ = Math.random()/100.0*focus.getDirY();
+
+			//move object
+			focusMesh.translateX(randX);
+			focusMesh.translateY(randY);
+			focusMesh.translateZ(randZ);
+
+			//check bound box for next it
+			if (focus.checkBoundingBox('x', focusMesh.position.x) == false){
+				focus.setDirX(focus.getDirX()*-1);
+			}
+			if(focus.checkBoundingBox('y', focusMesh.position.y) == false){
+				focus.setDirY(focus.getDirY()*-1);
+			}
+			if(focus.checkBoundingBox('z', focusMesh.position.z) == false){
+				focus.setDirZ(focus.getDirZ()*-1);
+			}
+
+		}
+	},
+
+	handleEvents : function(keyCode){
+		switch(keyCode){
+			case 38: //tilt up with 'up'
+				splatScene.camera.rotation.x += .01;
+				splatScene.camera.updateProjectionMatrix();
+				//alert('move forward!');
+				break;
+			case 40: //tilt down with 'down'
+				splatScene.camera.rotation.x -= .01;
+				splatScene.camera.updateProjectionMatrix();
+				break;
+			case 37: //look left with 'left'
+				splatScene.camera.rotation.y += .05;  
+				splatScene.camera.updateProjectionMatrix();
+				break;
+			case 39: //look right with 'right'
+				splatScene.camera.rotation.y-= .05;
+				splatScene.camera.updateProjectionMatrix();
+				break;
+			case 70: //move forward with 'f'
+				splatScene.camera.translateZ(-.1);
+				splatScene.camera.updateProjectionMatrix();
+				break;
+			case 66: //move backward with 'b'
+				splatScene.camera.translateZ(.1);
+				splatScene.camera.updateProjectionMatrix();
+				break;
+			default:
+				//alert(keyCode);
+		}
+	},
+	
+	// //Function to render scene
+	render: function(){
+		requestAnimationFrame(splatScene.render);
+
+		//if( splatScene.video.readyState === splatScene.video.HAVE_ENOUGH_DATA ){
+  		//	splatScene.videoTexture.needsUpdate = true;
+		//}
+
+		for(var i = 0; i<splatScene.objects.length; i++){
+			var focus = splatScene.objects[i];
+			splatScene.animations.rotate(focus);
+			splatScene.animations.translate(focus);
+			if(splatScene.objects[i].getCamera()){
+				splatScene.objects[i].getMesh().visible = false;
+				splatScene.objects[i].getCamera().updateCubeMap( splatScene.renderer, splatScene.scene );
+				splatScene.objects[i].getMesh().visible = true;
+			}
+		}
+
+		splatScene.renderer.render(splatScene.scene, splatScene.camera);
+	}
+	
+};
+
+splatScene.init();
+
+
+window.addEventListener('keydown', function(event) {
+	splatScene.handleEvents(event.keyCode);
+});
